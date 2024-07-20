01e58996fb6ba8cc75dc7e90bcf78c13d8c92c15
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-07-19 23:21:16 -0300

    Criei o superuser

--- bibliotech/admin.py
+++ bibliotech/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from bibliotech.models import Author, Category, Book, Emprestimo
 
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    ...
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+class BookAdmin(admin.ModelAdmin):
+    ...
+
+class EmprestimoAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    ...
