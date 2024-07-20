8a6c893102b089313db053c81e45673b32d142a6
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-07-20 00:06:52 -0300

    criando a pagina de empestimo

--- bibliotech/urls.py
+++ bibliotech/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'livros'
+
 urlpatterns = [
     path('', views.home, name="home"),
+    path('emprestimo/', views.emprestimo, name="emprestimo"),
+    path('livros/search/', views.search, name="search"),
+    path('livros/category/<int:category_id>/', views.category, name="category"),
+    path('livros/<int:id>/', views.book, name="livro"),
 ]
--- bibliotech/views.py
+++ bibliotech/views.py
@@ -1,7 +1,53 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+
+from bibliotech.models import Book
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Ola")
+    livros = Book.objects.all().order_by('-id')
+    return render (request, 'bibliotech/pages/home.html', context={
+        'livros': livros,
+    })
+
+def emprestimo(request):
+    livros = Book.objects.all().order_by('-id')
+    return render (request, 'bibliotech/pages/emprestimo.html', context={
+        'livros': livros,
+    })
+
+def category(request, category_id):
+    livros = get_list_or_404(
+        Book.objects.all().filter(
+            category__id=category_id
+        ).order_by('-id')
+    )
+
+    return render(request, 'bibliotech/pages/category.html', context={
+        'livros': livros,
+        'title': f'{book[0].category.name}'
+    })
+
+def book(request, id):
+    livro = get_object_or_404(Book, id)
+
+    return render(request, 'bibliotech/pages/livro.html', context={
+        'livro': livro,
+        'is_detail_page': True
+    })
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404
+    
+    livros = Book.objects.filter(
+        title__icontains=search_term
+    ).order_by('-id')
+
+    return render(request, 'bibliotech/pages/search.html', context={
+        'page_title': f'Search for {search_term} |',
+        'books': livros,
+    })
