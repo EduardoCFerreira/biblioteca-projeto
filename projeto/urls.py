de0dd2a0132778e248090bc001264a20ff0a43cc
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-08-07 21:04:41 -0300

    adicionando o app de usuarios e fazendo as configuracoes iniciais

--- projeto/urls.py
+++ projeto/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bibliotech.urls')),
-    path('usuarios/', include('usuarios.urls')),
+    path('usuarios/', include('usuarios.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
