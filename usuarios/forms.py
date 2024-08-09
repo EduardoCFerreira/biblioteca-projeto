c2ffd66cc6b79756e4d378c3fca73207ff1b55ab
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-08-08 21:39:23 -0300

    fazendo uma validação simples

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
 
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -73,3 +74,30 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Escreva sua senha aqui'
             })
     }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(pipoca)s no campo de senha',
+                code='invalid',
+                params= {'pipoca': '"atenção"'}
+            )
+        
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+
+        if 'Eduardo' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo de nome',
+                code = 'invalid',
+                params= {'value': '"Eduardo"'}
+            )
+        
+        return data
+    
+
+    
